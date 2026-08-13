0b71e4dcaba9a04ced70689a100b623f8bf098d2
Renaat Debleu <rdebleu@eWallah.net>
2026-08-13 10:06:32 +0000

    Version update

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'report_payments';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v5.4.2';
-$plugin->version = 2026060600;
+$plugin->release = 'v5.4.3';
+$plugin->version = 2026081300;
 $plugin->requires = 2025100600;
 $plugin->supported = [501, 502];
